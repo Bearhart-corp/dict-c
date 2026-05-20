2632bc3fdc4e157cc3fbf33ba5a202e77d9704a2
tom <thomas@Laptop-Niglo.localdomain>
2026-05-20 10:36:03 +0200

    hjcdfkhvjd

---
+++ md5.c
@@ -0,0 +1,106 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+uint32_t rol(uint32_t x, uint32_t s)
+{
+    s &= 31;
+    return (x << s) | (x >> (32 - s));
+}
+
+uint32_t F(uint32_t B, uint32_t C, uint32_t D, uint32_t *key, int i)
+{
+    if (i < 16)
+        return ((B & C) | (~B & D)) + key[i];
+    if (i >= 16 && i <= 31)
+        return ((B & D) | (C & ~D)) + key[(5 * i) & 15];
+    if (i >= 32 && i <= 47)
+        return (B ^ C ^ D) + key[(3 * i) & 15];;
+    if (i > 48)
+        return (C ^ (B | ~D)) + key[(7 * i) & 15];;
+}
+
+
+static void padding(const char *s, uint32_t *table)
+{
+    int i = 0;
+    int j = 0;
+    while (j < 14)
+    {
+        if (s[i])
+        {
+            j = i >> 2;
+            table[j] |= s[i] << ((i & 0b11) << 3);
+            i++;
+        }
+        else
+            table[++j] = 0;
+    }
+    table[14] = 0x80;
+    table[15] = i * 8;
+}
+
+static void hash(uint32_t *key, uint32_t *hacher)
+{
+    uint32_t a = 0x67452301;
+    uint32_t b = 0xefcdab89;
+    uint32_t c = 0x98badcfe;
+    uint32_t d = 0x10325476;
+
+    const uint32_t k[64] = {
+    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+    0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+    0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+
+    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+    0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+    0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+
+    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+    0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+    0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+
+    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+    0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+    0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+    };
+    int s_rotation[64] = {7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
+                5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
+                4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
+                6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21};
+
+    int i = -1;
+    uint32_t tmp;
+
+    while (++i < 64)
+    {
+        tmp = c;
+        c = b;
+        b = b +  rol(a + F(b, c, d, key, i) + k[i], s_rotation[i]);
+        a = d;
+        d = tmp;
+    }
+    hacher[0] = a;
+    hacher[1] = b;
+    hacher[2] = c;
+    hacher[3] = d;
+}
+
+int main(void)
+{
+    uint32_t x = 0x12345678;
+    uint32_t key[16];
+    uint32_t hacher[4];
+
+    padding("abcde", key);
+    hash(key, hacher);
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%x", hacher[i]);
+        printf("_");
+    }
+}
